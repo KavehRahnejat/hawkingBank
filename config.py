2fdf070b29b8a432b0746f1668380808fc43d06e
Rahnejat <krhn@UKLOC02XF3BT.local>
2018-12-07 17:49:07 +0000

    blah

--- config.py
+++ config.py
@@ -9,6 +9,7 @@
 """
 
 import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:  # pylint: disable=too-few-public-methods
@@ -18,3 +19,7 @@ class Config:  # pylint: disable=too-few-public-methods
     """
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
