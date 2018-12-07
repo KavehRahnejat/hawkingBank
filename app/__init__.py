6f48e22733cfb479b189986c38282206d0158fd1
Rahnejat <krhn@UKLOC02XF3BT.local>
2018-12-07 12:03:51 +0000

    testing linter

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
+"""
+    Here you should import any routes / apis you want to have included in the
+    application.
+
+"""
+
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # pylint: disable=invalid-name
 
-from app import routes
+from app import routes  # pylint: disable=unused-import, wrong-import-position
--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 
 """
 
-from app import app  #pylint: disable=unused-import
+from app import app  # pylint: disable=unused-import
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000)
