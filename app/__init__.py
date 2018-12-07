eed735c9455c08c425bb8eca583554fb65ed9ddd
Rahnejat <krhn@UKLOC02XF3BT.local>
2018-12-07 18:01:38 +0000

    now I remember why I dislike SQL alchemy....

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,13 @@
 # pylint: disable=missing-docstring
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 app = Flask(__name__)  # pylint: disable=invalid-name
 app.config.from_object(Config)
+db = SQLAlchemy(app)  # pylint: disable=invalid-name
+migrate = Migrate(app, db)  # pylint: disable=invalid-name
 
-from app import routes  # pylint: disable=unused-import, wrong-import-position
+from app import routes, models  # pylint: disable=unused-import, wrong-import-position
