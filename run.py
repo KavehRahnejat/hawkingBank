0de6d32a6e09d91bcb561a3cd37dae5d977a5728
Rahnejat <krhn@UKLOC02XF3BT.local>
2018-12-07 11:52:23 +0000

    testing linter

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
-from app import app
+"""
+    Welcome to hawkingBank!
+
+"""
+
+from app import app  #pylint: disable=unused-import
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
